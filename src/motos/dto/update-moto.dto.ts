e0e8dbc859ed2dcbc0fa7e0b2200efbb915b8f25
Pito <valentin.poveda.amaral@gmail.com>
2024-02-28 18:09:36 +0100

    modif

--- src/motos/dto/update-moto.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMotoDto } from './create-moto.dto';
-
-export class UpdateMotoDto extends PartialType(CreateMotoDto) {}
--- src/motos/motos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MotosService } from './motos.service';
-import { MotosController } from './motos.controller';
-
-@Module({
-  controllers: [MotosController],
-  providers: [MotosService],
-})
-export class MotosModule {}
